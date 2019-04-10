f1783a5e34ff65ebfe0ebf13e250d9cad35c6982
Artur-Galushka <49447125+Artur-Galushka@users.noreply.github.com>
2019-04-10 13:15:24 +0100

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,15 @@
+//
+//Operators
+//
+
+let first = 5,
+    second = 3;
+    
+let answer = first / second, //various operators in action
+    answer2 = first % second,
+    answer3 = first - second,
+    answer4 = first + second,
+    answer5 = first * second,
+    answer6 = (42 + first) * second;
+
+console.log(answer, answer2, answer3, answer4, answer5, answer6);
