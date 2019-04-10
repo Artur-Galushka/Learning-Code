acb43084d08181be2f0e9432f322cafa595221e5
Artur-Galushka <49447125+Artur-Galushka@users.noreply.github.com>
2019-04-10 13:15:10 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+Variables
+Training
+*/
+let productName = "Hammer",
+    ownerName = 'John\'s Hammer';
+let price = 5.00,
+    price2 = 3.00;
+let firstName = "John",
+    lastName = "Doe";
+console.log(productName, price + price2);
+console.log(ownerName);
+console.log(firstName + " " + lastName);
