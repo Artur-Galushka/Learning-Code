fbadc5077979d966eeb8f95a6b768dd7608d1b23
Artur-Galushka <49447125+Artur-Galushka@users.noreply.github.com>
2019-04-14 22:03:48 +0100

    Create types.js

---
+++ javascript/types.js
@@ -0,0 +1,14 @@
+
+let value = "apple",
+    value2 = 9,
+    value3 = "10",
+    value4 = 50/0; // 10/0 = Infinity, -10/0 = -Inf, 0/0 = NaN (Not a Number)
+let isMulti; //undefined "undefined"
+isMulti = null; //null "object"
+/*
+console.log(typeof(value));
+console.log(typeof(value2));
+console.log(typeof(value3));
+console.log(value4, typeof(value4));
+*/
+console.log(isMulti, typeof(isMulti));
