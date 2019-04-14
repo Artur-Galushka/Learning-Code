5b6304846f8c762a6cb0ddc2bcbaed225eaae031
Artur-Galushka <49447125+Artur-Galushka@users.noreply.github.com>
2019-04-14 22:02:19 +0100

    Update blackjack.js

--- javascript/blackjack.js
+++ javascript/blackjack.js
@@ -1,7 +1,9 @@
 //
 //Application blackjack
 //Copyright, license, name, etc.
+//
 
+/*
 let card1 = "Ace of Spades",
     card2 = "Ten of Hearts";
     
@@ -10,3 +12,56 @@ console.log("Welcome to Blackjack!");
 console.log("You are dealt: ");
 console.log(" " + card1);
 console.log(" " + card2);
+*/
+
+/*
+let deck = [
+  "Ace of Spades",
+  "Two of Spades",
+  "Three of Spades"
+];
+
+let playerCards = [
+  deck[0], 
+  deck[2]
+];
+
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
+*/
+
+let suits = [ "Hearts", "Spades", "Clubs", "Diamonds" ];
+let values = [ "Ace", "King", "Queen", "Jack",
+               "Ten", "Nine", "Eight", "Seven", 
+               "Six", "Five", "Four", "Three", "Two" ];
+
+function createDeck(){
+  let deck = [];
+  for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+      deck.push(values[valueIdx] + " of " + suits[suitIdx]);
+    }
+  }
+  return deck;
+}
+
+function getNextCard() {
+  return deck.shift();
+}
+
+let deck = createDeck();
+
+for (let i = 0; i < deck.length; i++) {
+  console.log(deck[i]);
+}
+
+let playerCards = [getNextCard(), getNextCard()];
+
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
