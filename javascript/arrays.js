7c0efa15b7ec015657e8a0a5c3e26d9ae7cab7fa
Artur-Galushka <49447125+Artur-Galushka@users.noreply.github.com>
2019-04-14 22:02:53 +0100

    Create arrays.js

---
+++ javascript/arrays.js
@@ -0,0 +1,12 @@
+
+let cities = ['New York', 'LA', 'Chicago'],
+    numbers = [0, 1, 2, 3, "America"];
+numbers.push(44); //add values to an array by .push
+numbers.push("5u");
+let resultS = numbers.pop(); //removes last value from an array
+
+/*
+console.log(cities[0], cities[1], cities[2], cities[3]);
+console.log(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]);
+*/
+console.log(resultS, numbers.length); //number of values in an array
